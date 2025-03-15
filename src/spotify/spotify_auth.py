db9068545445fe81b93ae7b95cbed5198c3a2504
Dani Castellanos <125296144+Daniela-Castellanos@users.noreply.github.com>
2025-03-14 19:00:20 -0600

    Update spotify_auth.py

--- src/spotify/spotify_auth.py
+++ src/spotify/spotify_auth.py
@@ -3,8 +3,8 @@ from spotipy.oauth2 import SpotifyOAuth
 
 def get_spotify_client():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-        client_id="e8924ad5551f47098c6633a842da8bd6",
-        client_secret="e26d8ac769614f8ab9bcc6498496b5d3",
+        client_id="0000",
+        client_secret="0000",
         redirect_uri="http://localhost:5000/callback",
         scope=["user-library-read", "user-top-read", "user-read-playback-state", "user-read-recently-played"]
     ))
