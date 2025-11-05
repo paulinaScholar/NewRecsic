8efe81c12b7ddcc4afc817130a76ec2e288eaa24
Paulina Campos Chavez <142939712+paulinaScholar@users.noreply.github.com>
2025-11-04 19:40:10 -0600

    Update spotify_auth.py

--- src/spotify/spotify_auth.py
+++ src/spotify/spotify_auth.py
@@ -6,7 +6,7 @@ def get_spotify_client():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
         client_id=os.getenv("CLIENT_ID"),
         client_secret=os.getenv("CLIENT_SECRET"),
-        redirect_uri="http://localhost:5000/callback",
+        redirect_uri="http://127.0.0.1:8050/callback",
         scope=["user-library-read", "user-top-read", "user-read-playback-state", "user-read-recently-played"]
     ))
     return sp
